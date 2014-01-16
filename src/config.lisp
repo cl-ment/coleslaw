7065bf4ca6400c4d1f51f1b3aa4f555ce943f324
Clement Gerouville <cl-ment@gerouville.fr>
2014-01-16 16:59:17 +0000

    Enabled UTF-8 when reading the configuration file

--- src/config.lisp
+++ src/config.lisp
@@ -42,7 +42,7 @@ are in the plugins folder in coleslaw's source directory."
 (defun load-config (&optional config-key (dir (user-homedir-pathname)))
   "Load the coleslaw configuration from DIR/.coleslawrc, using CONFIG-KEY
 if necessary. DIR is ~ by default."
-  (with-open-file (in (merge-pathnames ".coleslawrc" dir))
+  (with-open-file (in (merge-pathnames ".coleslawrc" dir) :external-format '(:utf-8))
     (let ((config-form (read in)))
       (if (symbolp (car config-form))
           ;; Single site config: ignore CONFIG-KEY.
